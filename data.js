6fae4fb49c397d4d1bec5f88f031889c35a1f279
MartinaEngler <martinafrank@bluewin.ch>
2026-07-23 15:25:24 +0200

    Refactor categories array for consistency

--- data.js
+++ data.js
@@ -1,76 +1,76 @@
 const categories = [
 
-{
-  name: "Dokumente",
-  items: [
-    "Personalausweis",
-    "Führerschein",
-    "Fahrzeugausweis",
-    "Versicherung",
-    "Campingreservation"
-  ]
-},
+  {
+    name: "Dokumente",
+    items: [
+      "Personalausweis",
+      "Führerschein",
+      "Fahrzeugausweis",
+      "Versicherung",
+      "Campingreservation"
+    ]
+  },
 
-{
-  name: "Küche",
-  items: [
-    "Teller",
-    "Becher",
-    "Besteck",
-    "Pfanne",
-    "Kochtopf",
-    "Kochlöffel",
-    "Kaffee",
-    "Kaffeefilter"
-  ]
-},
+  {
+    name: "Küche",
+    items: [
+      "Teller",
+      "Becher",
+      "Besteck",
+      "Pfanne",
+      "Kochtopf",
+      "Kochlöffel",
+      "Kaffee",
+      "Kaffeefilter"
+    ]
+  },
 
-{
-  name: "Kleidung",
-  items: [
-    "T-Shirts",
-    "Pullover",
-    "Hosen",
-    "Unterwäsche",
-    "Socken",
-    "Jacke",
-    "Pyjama"
-  ]
-},
+  {
+    name: "Kleidung",
+    items: [
+      "T-Shirts",
+      "Pullover",
+      "Hosen",
+      "Unterwäsche",
+      "Socken",
+      "Jacke",
+      "Pyjama"
+    ]
+  },
 
-{
-  name: "Bad",
-  items: [
-    "Zahnbürste",
-    "Zahnpasta",
-    "Shampoo",
-    "Duschgel",
-    "Handtücher"
-  ]
-},
+  {
+    name: "Bad",
+    items: [
+      "Zahnbürste",
+      "Zahnpasta",
+      "Shampoo",
+      "Duschgel",
+      "Handtücher"
+    ]
+  },
 
-{
-  name: "Camping",
-  items: [
-    "Campingtisch",
-    "Campingstühle",
-    "Stromkabel",
-    "CEE-Adapter",
-    "Wasserschlauch",
-    "Gießkanne",
-    "Taschenlampe"
-  ]
-},
+  {
+    name: "Camping",
+    items: [
+      "Campingtisch",
+      "Campingstühle",
+      "Stromkabel",
+      "CEE-Adapter",
+      "Wasserschlauch",
+      "Gießkanne",
+      "Taschenlampe"
+    ]
+  },
 
-{
-  name: "Hund",
-  items: [
-    "Hundefutter",
-    "Leine",
-    "Geschirr",
-    "Napf",
-    "Kotbeutel"
-  ]
-}
+  {
+    name: "Hund",
+    items: [
+      "Hundefutter",
+      "Leine",
+      "Geschirr",
+      "Napf",
+      "Kotbeutel"
+    ]
+  }
 
 ];
