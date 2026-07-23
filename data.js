b204fc874f04e5e690d7ad04804c4ea97b8f3454
MartinaEngler <martinafrank@bluewin.ch>
2026-07-23 13:51:13 +0200

    Add categories and items to data.js

---
+++ data.js
@@ -0,0 +1,121 @@
+const categories = [
+  {
+    name: "📄 Dokumente",
+    items: [
+      "Ausweis",
+      "Führerschein",
+      "Fahrzeugausweis",
+      "Versicherung",
+      "EC-/Kreditkarte",
+      "Bargeld"
+    ]
+  },
+  {
+    name: "👕 Kleidung",
+    items: [
+      "T-Shirts",
+      "Pullover",
+      "Hosen",
+      "Unterwäsche",
+      "Socken",
+      "Jacke",
+      "Pyjama",
+      "Schuhe"
+    ]
+  },
+  {
+    name: "🚿 Bad & Hygiene",
+    items: [
+      "Zahnbürste",
+      "Zahnpasta",
+      "Duschgel",
+      "Shampoo",
+      "Handtücher",
+      "Kamm"
+    ]
+  },
+  {
+    name: "💊 Apotheke",
+    items: [
+      "Pflaster",
+      "Schmerzmittel",
+      "Desinfektionsmittel",
+      "Persönliche Medikamente"
+    ]
+  },
+  {
+    name: "🍳 Küche",
+    items: [
+      "Pfanne",
+      "Topf",
+      "Besteck",
+      "Teller",
+      "Tassen",
+      "Kaffeekocher"
+    ]
+  },
+  {
+    name: "🥫 Lebensmittel",
+    items: [
+      "Nudeln",
+      "Reis",
+      "Gewürze",
+      "Öl",
+      "Kaffee",
+      "Milch"
+    ]
+  },
+  {
+    name: "🥤 Getränke",
+    items: [
+      "Mineralwasser",
+      "Sirup",
+      "Prosecco"
+    ]
+  },
+  {
+    name: "⛺ Camping",
+    items: [
+      "Campingtisch",
+      "Campingstühle",
+      "Stromkabel",
+      "Adapter",
+      "Markise"
+    ]
+  },
+  {
+    name: "🔌 Technik",
+    items: [
+      "Handyladegerät",
+      "Powerbank",
+      "AirPods",
+      "USB-Kabel"
+    ]
+  },
+  {
+    name: "🐶 Hund",
+    items: [
+      "Hundefutter",
+      "Leine",
+      "Napf",
+      "Hundedecke"
+    ]
+  },
+  {
+    name: "🎲 Freizeit",
+    items: [
+      "Bücher",
+      "Spielkarten",
+      "Velos"
+    ]
+  },
+  {
+    name: "📦 Sonstiges",
+    items: [
+      "Sonnenbrille",
+      "Ohropax",
+      "Kissen",
+      "Decken"
+    ]
+  }
+];
